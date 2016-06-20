b439153e1babde95a4ae071b8892e84929cdf977
Leo Rubiano <leo.a.rubiano@gmail.com>
2016-06-20 15:44:27 -0400

    added a few more dict keys

---
+++ players_collection.py
@@ -0,0 +1,203 @@
+#part 1: import players from CSV file into dictionary
+
+p1 = {
+	'name' : 'Joe Smith',
+	'height' : 42,
+	'experience' : True,
+	'guardian_1' : 'Jim Smith',
+	'guardian_2' : 'Jan Smith',
+	'team' : '',
+	'assigned' : False
+}
+
+p2 = {
+	'name' : 'Jill Tanner',
+	'height' : 36,
+	'experience' : True,
+	'guardian_1' : 'Clara Tanner',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p3 = {
+	'name' : 'Bill Bon',
+	'height' : 43,
+	'experience' : True,
+	'guardian_1' : 'Sara Bon',
+	'guardian_2' : 'Jenny Bon',
+	'team' : '',
+	'assigned' : False
+}
+
+p4 = {
+	'name' : 'Eva Gordon',
+	'height' : 45,
+	'experience' : False,
+	'guardian_1' : 'Wendy Gordon',
+	'guardian_2' : 'Mike Gordon',
+	'team' : '',
+	'assigned' : False
+}
+
+p5 = {
+	'name' : 'Matt Gill',
+	'height' : 40,
+	'experience' : False,
+	'guardian_1' : 'Charles Gill',
+	'guardian_2' : 'Sylvia Gill',
+	'team' : '',
+	'assigned' : False
+}
+
+p6 = {
+	'name' : 'Kimmy Stein',
+	'height' : 41,
+	'experience' : False,
+	'guardian_1' : 'Bill Stein',
+	'guardian_2' : 'Hillary Stein',
+	'team' : '',
+	'assigned' : False
+}
+
+p7 = {
+	'name' : 'Sammy Adams',
+	'height' : 45,
+	'experience' : False,
+	'guardian_1' : 'Jeff Adams',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p8 = {
+	'name' : 'Karl Saygan',
+	'height' : 42,
+	'experience' : True,
+	'guardian_1' : 'Heather Bledsoe',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p9 = {
+	'name' : 'Suzane Greenberg',
+	'height' : 44,
+	'experience' : True,
+	'guardian_1' : 'Henrietta Dumas',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p10 = {
+	'name' : 'Sal Dali',
+	'height' : 41,
+	'experience' : False,
+	'guardian_1' : 'Gala Dali',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p11 = {
+	'name' : 'Joe Kavalier',
+	'height' : 39,
+	'experience' : False,
+	'guardian_1' : 'Sam Kavalier',
+	'guardian_2' : 'Elaine Kavalier',
+	'team' : '',
+	'assigned' : False
+}
+
+p12 = {
+	'name' : 'Ben Finkelstein',
+	'height' : 44,
+	'experience' : False,
+	'guardian_1' : 'Aaron Finkelstein',
+	'guardian_2' : 'Jill Finkelstein',
+	'team' : '',
+	'assigned' : False
+}
+
+p13 = {
+	'name' : 'Diego Soto',
+	'height' : 41,
+	'experience' : True,
+	'guardian_1' : 'Robin Soto',
+	'guardian_2' : 'Sarika Soto',
+	'team' : '',
+	'assigned' : False
+}
+
+p14 = {
+	'name' : 'Chloe Alaska',
+	'height' : 47,
+	'experience' : False,
+	'guardian_1' : 'David Alaska',
+	'guardian_2' : 'Jamie Alaska',
+	'team' : '',
+	'assigned' : False
+}
+
+p15 = {
+	'name' : 'Arnold Willis',
+	'height' : 43,
+	'experience' : False,
+	'guardian_1' : 'Claire Willis',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p16 = {
+	'name' : 'Phillip Helm',
+	'height' : 44,
+	'experience' : True,
+	'guardian_1' : 'Thomas Helm',
+	'guardian_2' : 'Evan Jones',
+	'team' : '',
+	'assigned' : False
+}
+
+p17 = {
+	'name' : 'Les Clay',
+	'height' : 42,
+	'experience' : True,
+	'guardian_1' : 'Wynonna Brown',
+	'guardian_2' : '',
+	'team' : '',
+	'assigned' : False
+}
+
+p18 = {
+	'name' : 'Herschel Krustofski',
+	'height' : 45,
+	'experience' : True,
+	'guardian_1' : 'Hyman Krustofski',
+	'guardian_2' : 'Rachel Krustofski',
+	'team' : '',
+	'assigned' : False
+}
+
+
+all_players = [
+	p1, 
+	p2, 
+	p3, 
+	p4, 
+	p5 , 
+	p6, 
+	p7, 
+	p8, 
+	p9,
+	p10,
+	p11,
+	p12,
+	p13,
+	p14,
+	p15,
+	p16,
+	p17,
+	p18
+	]
